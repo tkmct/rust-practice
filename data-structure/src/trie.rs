63b6b8cf47972759c3e076ffc5a7dc00b19615f9
Takamichi Tsutsumi <1220t.takamichi@gmail.com>
2019-12-28 15:51:15 +0900

    add lower alphabetic trie

--- data-structure/src/trie.rs
+++ data-structure/src/trie.rs
@@ -1,3 +1,16 @@
+const ASCII_A: usize = 97;
+
+fn get_digit(c: char) -> Option<usize> {
+    match c as usize {
+        97..=122 => Some((c as usize) - ASCII_A),
+        _ => None,
+    }
+}
+
+fn is_lower_alphabet(s: &str) -> bool {
+    s.chars().all(|c| get_digit(c).is_some())
+}
+
 struct Node {
     children: [Option<Box<Node>>; 26],
     is_end_of_word: bool,
@@ -12,15 +25,59 @@ impl Node {
             is_end_of_word: false,
         }
     }
+
+    fn append_child(&mut self, i: usize) {
+        assert!(i <= 25);
+        self.children[i] = Some(Box::new(Node::new()));
+    }
+
+    fn insert(&mut self, s: &str) {
+        let c = s.chars().nth(0);
+        if c.is_none() {
+            self.is_end_of_word = true;
+            return;
+        }
+
+        let s = &s[1..];
+        let n = get_digit(c.unwrap()).unwrap();
+        self.append_child(n);
+        self.children[n].as_mut().unwrap().insert(s);
+    }
+
+    fn search(&self, s: &str) -> bool {
+        let c = s.chars().nth(0);
+        if c.is_none() {
+            return self.is_end_of_word;
+        }
+
+        let s = &s[1..];
+        let n = get_digit(c.unwrap()).unwrap();
+        let child = &self.children[n];
+        if child.is_none() {
+            return false;
+        }
+
+        child.as_ref().unwrap().search(s)
+    }
 }
 
-struct Tree {
+struct Trie {
     root: Node,
 }
 
-impl Tree {
-    fn new() -> Self {
+impl Trie {
+    pub fn new() -> Self {
         Self { root: Node::new() }
+    }
+
+    pub fn insert(&mut self, s: &str) {
+        assert!(is_lower_alphabet(s));
+        self.root.insert(s);
+    }
+
+    pub fn search(&self, s: &str) -> bool {
+        assert!(is_lower_alphabet(s));
+        self.root.search(s)
     }
 }
 
@@ -29,5 +86,35 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test() {}
+    fn test_get_digit() {
+        assert_eq!(get_digit('a'), Some(0));
+        assert_eq!(get_digit('z'), Some(25));
+        assert_eq!(get_digit('-'), None);
+    }
+
+    #[test]
+    fn test_is_lower_alphabet() {
+        assert!(is_lower_alphabet("helloworld"));
+        assert!(!is_lower_alphabet("HelloWorld"));
+    }
+
+    #[test]
+    fn test_trie() {
+        let mut trie = Trie::new();
+
+        trie.insert("hello");
+        trie.insert("world");
+        assert!(trie.search("hello"));
+        assert!(!trie.search("hi"));
+    }
+
+    #[test]
+    fn test_trie2() {
+        let mut trie = Trie::new();
+        trie.insert("there");
+        trie.insert("their");
+
+        assert!(trie.search("their"));
+        assert!(!trie.search("thei"));
+    }
 }
